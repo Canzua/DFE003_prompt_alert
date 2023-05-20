edde2b34414aa7714a6dd0c466ed009b73236579
Andrea dos Santos <contato.andreadossantos@gmail.com>
2023-05-20 18:32:01 -0300

    exercicio 1

---
+++ src/main.js
@@ -0,0 +1,13 @@
+
+const number = prompt('Oi, por favor insira um número que eu te falo quanto é o dobro Lo)');
+const doble = number * 2;
+
+alert("O dobro do número informado é: " + doble);
+
+
+
+
+
+
+
+

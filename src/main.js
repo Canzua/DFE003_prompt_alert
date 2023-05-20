2f9420468222ba35aaaf42a10be8306093993757
Andrea dos Santos <contato.andreadossantos@gmail.com>
2023-05-20 18:35:13 -0300

    exercicio 2

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ const doble = number * 2;
 
 alert("O dobro do número informado é: " + doble);
 
+const restaurantBillAmount = prompt('Agora é a hora de acertar a conta, qual o valor do jantar?' );
+const billAmountNumber =  parseInt(restaurantBillAmount);
+const rate = 10;
+const callRate = ((rate * billAmountNumber) / 100);
+const total = (billAmountNumber + callRate);
 
-
-
-
-
-
+alert("O valor total do jantar, incluindo a taxa de atendimento, é: R$ " + total);
 
